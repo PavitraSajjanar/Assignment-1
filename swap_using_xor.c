4bb7e738a22bc539988b95c9d352117ba8257684
PavitraSajjanar <37983198+PavitraSajjanar@users.noreply.github.com>
2020-01-16 19:30:49 +0530

    modified code
    code is written in user friendly way

--- swap_using_xor.c
+++ swap_using_xor.c
@@ -5,13 +5,22 @@ int main()
 {
     int a;
     int b;
+
+    printf("Code to swap two numbers using 'xor' operator \n");
+    printf("Enter two numbers\n");
+
     scanf("%d %d", &a, &b);
+
+    printf("\nThe numbers before swapping: \n");
+    printf("a: %d\tb: %d\n", a, b);
 
     a = a ^ b;
     b = a ^ b;
     a = a ^ b;
-    
+
+    printf("\nThe numbers after swapping: \n");
     printf("a: %d\tb: %d\n", a, b);
 
     return 0;
 }
+
